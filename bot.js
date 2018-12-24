d877d64e55d90f76761df97aed873a18d2368ac2
hamzaxo0 <46108526+hamzaxo0@users.noreply.github.com>
2018-12-24 19:22:28 +0100

    Update and rename bot (4).js to bot.js

---
+++ bot.js
@@ -0,0 +1,93 @@
+const Discord = require('discord.js'),
+
+    client = new Discord.Client({sisableEveryone: true})
+
+    console.log("تم التشغيل,");
+
+client.on('guildMemberAdd', member => {
+
+const mohamed= member.guild.channels.get("524931094831955968");
+
+if(!mohamed) return;
+
+if(mohamed) {
+
+setTimeout(() => mohamed.send(`**Ꮃelcome Ꭲo Ꮪerver Soul ..**`), 5000)        
+
+}
+
+});
+
+client.on('ready',async () => {
+
+  client.channels.find(ch => ch.id === "524959881493610539" && ch.type === 'voice').join();
+
+});
+
+
+
+const devs = ['523832987104837642' , '' , ''];
+
+const adminprefix = "S";
+
+client.on('message', message => {
+
+    var argresult = message.content.split(` `).slice(1).join(' ');
+
+      if (!devs.includes(message.author.id)) return;
+
+      
+
+  if (message.content.startsWith(adminprefix + 'ply')) {
+
+    client.user.setGame(argresult);
+
+      message.channel.sendMessage(`**  ${argresult} ply .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'wt')) {
+
+  client.user.setActivity(argresult, {type:'WATCHING'});
+
+      message.channel.sendMessage(`**  ${argresult} wt .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'ls')) {
+
+  client.user.setActivity(argresult , {type:'LISTENING'});
+
+      message.channel.sendMessage(`**  ${argresult} ls .. ✅**`)
+
+  } else 
+
+  if (message.content.startsWith(adminprefix + 'st')) {
+
+    client.user.setGame(argresult, "https://www.twitch.tv/muuuuuute");
+
+      message.channel.sendMessage(`**  ${argresult} st .. ✅**`)
+
+    }
+
+  if (message.content.startsWith(adminprefix + 'us')) {
+
+  client.user.setUsername(argresult).then
+
+      message.channel.send(`**us ..**${argresult}** `)
+
+  } else
+
+  if (message.content.startsWith(adminprefix + 'av')) {
+
+  client.user.setAvatar(argresult);
+
+    message.channel.send(`**avatar ... :** `);
+
+  }
+
+  });
+
+
+
+client.login(process.env.BOT_TOKEN);لا تغير فيها شيء
